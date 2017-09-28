f7b58a0db18dcf89fe76cfbc97eae3992fbda78f
Aaron Sikes <aaron@sikes.io>
2017-09-28 11:03:34 -0400

    Convert a stream to a promise for an array of lines

---
+++ collect-lines.js
@@ -0,0 +1,13 @@
+module.exports = function(stream) {
+  return new Promise(function(resolve, reject) {
+    stream.on('error', reject);
+    let chunks = [];
+    stream.on('data', (c) => chunks.push(c));
+    stream.on('end', function() {
+      let data = Buffer.concat(chunks).toString();
+      let lines = data.split('\n')
+        .map((line) => line.trim());
+      resolve(lines);
+    });
+  });
+}
---
+++ test/collect-lines.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const stream = require('stream');
+const collect = require('../collect-lines');
+
+describe("The stream lines collector", function() {
+  it("resolves to an array of lines", async function() {
+    let stdin = new stream.PassThrough();
+    stdin.write('a');
+    stdin.write('\nb\nc');
+    let lines = collect(stdin);
+    stdin.end('at');
+    assert.deepEqual(await lines, ['a', 'b', 'cat']);
+  });
+});
